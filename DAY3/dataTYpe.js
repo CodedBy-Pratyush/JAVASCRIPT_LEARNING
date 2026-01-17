3fc1eaf0e5a3f2e2c79012f43064f338097afdfc
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-17 11:16:42 +0530

    Data type

---
+++ DAY3/dataTYpe.js
@@ -0,0 +1,19 @@
+// DATA TYPES IN JAVASCRIPT
+
+// -------- Numbers --------
+let price = 49.75;
+let quantity = 4;
+console.log(price + quantity);   // Addition
+console.log(price * quantity);   // Multiplication
+
+// -------- Strings --------
+const userFirstName = "Aman";
+const userLastName = "Sharma";
+const instituteName = "IIT Delhi";
+
+console.log(userFirstName + " " + userLastName);
+console.log(instituteName);
+
+// -------- Booleans --------
+let isUserLoggedIn = true;
+console.log(isUserLoggedIn);
