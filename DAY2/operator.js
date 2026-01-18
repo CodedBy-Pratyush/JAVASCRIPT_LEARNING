77b5005d024b03327d420831da75df17a04caf15
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-18 19:50:46 +0530

    updated

---
+++ DAY2/operator.js
@@ -0,0 +1,9 @@
+// -------- Strings --------
+const firstName = "Aman";
+const surname = "Sharma";
+
+console.log(firstName + " " + surname);
+
+// -------- Numbers --------
+let valueA = 15;
+let valueB = 8;
