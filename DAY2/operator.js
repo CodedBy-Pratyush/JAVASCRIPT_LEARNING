6b8a52f697ee8a7ab30d25caa80888ab842f64a8
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-18 19:51:51 +0530

    update again

--- DAY2/operator.js
+++ DAY2/operator.js
@@ -7,3 +7,13 @@ console.log(firstName + " " + surname);
 // -------- Numbers --------
 let valueA = 15;
 let valueB = 8;
+
+console.log(valueA * valueB); // 120
+
+// -------- Boolean in condition --------
+if (1) {
+    console.log("Condition is TRUE");
+}
+
+// 1  -> true
+// 0  -> false
