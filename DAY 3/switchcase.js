a4d3de380806389815feb881cf0d629717b16004
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-19 22:50:39 +0530

    updated 4

---
+++ DAY 3/switchcase.js
@@ -0,0 +1,10 @@
+// If else ( Conditional Statements)
+
+const age = 2;
+if (age >= 80) {
+  //   console.log("Eligible");
+} else if (age >= 18) {
+  //   console.log("No cause YOur are to old");
+} else {
+  //   console.log("Not Eligible ");
+}
