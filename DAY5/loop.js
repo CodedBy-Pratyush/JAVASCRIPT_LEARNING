37ba85b9c65f69095baba31b427351463e930e58
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-20 22:00:16 +0530

    updated 5

--- DAY5/loop.js
+++ DAY5/loop.js
@@ -1,27 +1,19 @@
-// =====================
-// LOOPS IN JAVASCRIPT
-// =====================
+// Loops :  For | While | DO while
 
-
-// -------- FOR LOOP --------
-// Use when you KNOW how many iterations are needed
-
-for (let step = 0; step <= 50; step += 5) {
-  // Prints the name along with current step value
-  // console.log("Tejas", step);
+// For:   -> if you know how many times to loop
+for (let i = 0; i <= 50; i = i + 5) {
+  // console.log("Tejas" +  " " + i);
 }
 
+// While : know the conditon but don't know about how much time it take to loop
 
+let ip = 0;
+let house = 100;
 
-// -------- WHILE LOOP --------
-// Use when you know the CONDITION but NOT the number of iterations
-
-let currentPosition = 0;   // starting point
-let destination = 100;     // target point
-
-while (currentPosition !== destination) {
-  currentPosition++;       // move one step forward
-  // console.log("Step taken:", currentPosition);
+while (ip != house) {
+  ip = ip + 1;
+  // console.log("Step Taken" + ip);
 }
 
+// do while loop : first run the program and then cheack the condition
 
