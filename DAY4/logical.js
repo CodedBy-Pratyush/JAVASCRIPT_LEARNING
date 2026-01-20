fd271ce4c2e149e44e4100957b620aaae56957f6
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-20 21:47:19 +0530

    updated 4

---
+++ DAY4/logical.js
@@ -0,0 +1,13 @@
+// Logical operators
+  //( AND OR NOT)
+
+  // AND -> all conditon must be true && 
+
+  const age=12
+  const gender='male'
+
+  if(age>=18 && gender=='male')
+  {
+    // console.log("You're an Adult");
+  }
+  
