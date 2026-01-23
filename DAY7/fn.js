ff8e257bc4202a58996bfd124a541b56588d419d
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-23 13:08:05 +0530

    js file

--- DAY7/fn.js
+++ DAY7/fn.js
@@ -4,3 +4,11 @@ function sayHello() {
   // This function prints a greeting message in the console
   console.log("Hello This side Tejas");
 }
+// sayHello();  // Calling Of Function (currently commented to avoid auto execution)
+
+// parameter
+function add(a, b) {
+  // a and b are parameters received by the function
+  let ans = a + b; // storing sum of a and b
+  return ans; // returning final result
+}
