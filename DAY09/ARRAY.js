54a96e2c79cd21260e10d517c042af2320333fb1
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-27 23:33:09 +0530

    add

---
+++ DAY09/ARRAY.js
@@ -0,0 +1,16 @@
+// Arrays in Javascript 
+
+const users = ["Ravi", "Aman", "Soham", "Kunal"]; // creating an array
+
+console.log(users);
+// accessing the value
+
+// users[0] = "Ravi-Sharma";  // muted the value
+// console.log(users[0]);
+// console.log(users);
+
+
+users.push("Nova"); // adding value at the end
+// users.push("Echo"); 
+// console.log(users);
+
