eb74cfd43a56f21997252789a8cf0d65fe2ae7d1
CodedBy-Pratyush <pratyushbanerjee75@gmail.com>
2026-01-27 23:33:30 +0530

    add

--- DAY09/ARRAY.js
+++ DAY09/ARRAY.js
@@ -14,3 +14,25 @@ users.push("Nova"); // adding value at the end
 // users.push("Echo"); 
 // console.log(users);
 
+
+// Homogeneous -> ek type ke value add kr skte array me
+// but in js : koi bhi type ke value add kr skte
+
+// Heterogeneous value can be added
+const mixedData = ["Ravi", 98765, 221, true, "999"];
+// console.log(mixedData);
+// mixedData.push({ city: "Delhi" });
+// console.log(mixedData);
+
+// findindex
+
+// let position = mixedData.indexOf("999");
+// console.log(position);
+ 
+// removing last element from end of an array
+users.pop();
+console.log(users);
+
+// reverse the value from an array
+users.reverse();
+console.log(users);
